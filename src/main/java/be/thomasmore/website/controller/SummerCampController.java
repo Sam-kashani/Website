2ec3a0dc9171c0c41a8aac0f2bde761f27b38032
Sam <r0912982@student.thomasmore.be>
2025-05-29 20:09:43 +0200

    extra functie toegevoegd

--- src/main/java/be/thomasmore/website/controller/SummerCampController.java
+++ src/main/java/be/thomasmore/website/controller/SummerCampController.java
@@ -1,7 +1,10 @@
 package be.thomasmore.website.controller;
 
+import be.thomasmore.website.model.Participant;
 import be.thomasmore.website.model.SummerCamp;
 import be.thomasmore.website.model.Activity;
+import be.thomasmore.website.repositories.ParticipantRepository;
+import be.thomasmore.website.repositories.RegistrationRepository;
 import be.thomasmore.website.repositories.SummerCampRepository;
 import be.thomasmore.website.repositories.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,38 +23,55 @@ public class SummerCampController {
 
     @Autowired
     private SummerCampRepository summerCampRepository;
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+
+    @Autowired
+    private RegistrationRepository registrationRepository;
 
     @Autowired
     private ActivityRepository activityRepository;
 
     @GetMapping({"/campgreeting/{id}", "/campgreeting"})
-    public String campGreeting(Model model, @PathVariable(required = false) Integer id) {
+    public String campGreeting(@PathVariable(required = false) Integer id,
+                               Model model,
+                               Principal principal) {
+
         if (id == null) {
-            return "campgreeting";
+            return "campDetails";
         }
 
-        Optional<SummerCamp> campFromDb = summerCampRepository.findById(id);
-        model.addAttribute("camp", campFromDb.orElse(null));
+        SummerCamp camp = summerCampRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Kamp niet gevonden"));
+        model.addAttribute("camp", camp);
+        model.addAttribute("greeting", "Welkom bij " + camp.getName());
 
         int prevId = (id == 1) ? 10 : id - 1;
         int nextId = (id == 10) ? 1 : id + 1;
-
         model.addAttribute("prevId", prevId);
         model.addAttribute("nextId", nextId);
 
-        String greeting = campFromDb.map(c -> "Maak kennis met onze " + c.getName() + " in " + c.getLocation())
-                .orElse("Geen kamp gevonden.");
-        model.addAttribute("greeting", greeting);
-
-        Iterable<Activity> iterable = activityRepository.findAll();
-        List<Activity> activities = new ArrayList<>();
-        for (Activity a : iterable) {
-            activities.add(a);
-        }
+        List<Activity> activities = activityRepository.findBySummerCampId(id);
         model.addAttribute("activities", activities);
 
-        return "campgreeting";
+        boolean isRegistered = false;
+        if (principal != null) {
+            Optional<Participant> optionalParticipant = participantRepository.findByUsername(principal.getName());
+            if (optionalParticipant.isPresent()) {
+                Participant participant = optionalParticipant.get();
+                model.addAttribute("participant", participant);
+
+                isRegistered = registrationRepository.existsByParticipantAndCamp(participant, camp);
+            }
+        }
+        model.addAttribute("isRegistered", isRegistered);
+
+
+        return "campDetails";
     }
+
+
 
     @GetMapping("/camps")
     public String filterCamps(Model model,
@@ -63,6 +83,6 @@ public class SummerCampController {
 
         List<SummerCamp> camps = summerCampRepository.findByFilters(name, location, minParticipants, maxPrice, campType);
         model.addAttribute("camps", camps);
-        return "list";
+        return "allCamps";
     }
 }
