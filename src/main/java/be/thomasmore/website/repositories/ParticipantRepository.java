334a4472a38b2b1de55a2717d1ec0318f087f4c2
Sam <r0912982@student.thomasmore.be>
2025-05-29 20:04:39 +0200

    findByUsername toegevoegd

--- src/main/java/be/thomasmore/website/repositories/ParticipantRepository.java
+++ src/main/java/be/thomasmore/website/repositories/ParticipantRepository.java
@@ -4,5 +4,9 @@ import be.thomasmore.website.model.Participant;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ParticipantRepository extends CrudRepository<Participant, Integer> {
+    Optional<Participant> findByUsername(String username);
+
 }
