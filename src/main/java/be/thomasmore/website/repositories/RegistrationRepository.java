7b8a93502f50be86fcfa92877e1347a518bef65c
Sam <r0912982@student.thomasmore.be>
2025-05-29 20:06:41 +0200

    compleet aangepast, list's verwijderd en toegevoegd, boolean toegevoegd

--- src/main/java/be/thomasmore/website/repositories/RegistrationRepository.java
+++ src/main/java/be/thomasmore/website/repositories/RegistrationRepository.java
@@ -1,12 +1,18 @@
 package be.thomasmore.website.repositories;
 
+import be.thomasmore.website.model.Participant;
 import be.thomasmore.website.model.Registration;
+import be.thomasmore.website.model.SummerCamp;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
-    List<Registration> findBySummerCampId(Integer summerCampId);
-    List<Registration> findByParticipantId(Integer participantId);
+
+    boolean existsByParticipantAndCamp(Participant participant, SummerCamp camp);
+
+    Registration findByParticipantAndCamp(Participant participant, SummerCamp camp);
+
+    List<Registration> findByParticipant(Participant participant);
 }
 
